43d25e8cfd8d139424fb9f27c118d60d63e69f58
Gabriel Fernandes <hu3master.zord@hotmail.com>
2023-08-21 20:09:55 -0300

    finalizando encrypter

--- src/lib/EcrypteCryptor.ts
+++ src/lib/EcrypteCryptor.ts
@@ -8,7 +8,7 @@ export default class EncrypterCrypto implements Encrypter {
 
   private ivLength = 16;
 
-  private key = process.env.ENCRYPTION_KEY || 'Batata-monstruosa';
+  private key = process.env.ENCRYPTION_KEY || crypto.randomBytes(32);
   
   encrypt(password: string): string {
     const iv = crypto.randomBytes(this.ivLength);
@@ -17,12 +17,12 @@ export default class EncrypterCrypto implements Encrypter {
       .toString(this.encoding as BufferEncoding);
   }
 
-  compare(password: string): boolean {
-    const binaryData = Buffer.from(password, this.encoding as BufferEncoding);
+  compare(encyptedP: string, password:string): boolean {
+    const binaryData = Buffer.from(encyptedP, this.encoding as BufferEncoding);
     const iv = binaryData.slice(-this.ivLength);
     const encryptedData = binaryData.slice(0, binaryData.length - this.ivLength);
     const decipher = crypto.createDecipheriv(this.algorithm, Buffer.from(this.key), iv);
     const decoded = Buffer.concat([decipher.update(encryptedData), decipher.final()]).toString();
-    return password === decoded;
+    return decoded === password;
   }
 }
